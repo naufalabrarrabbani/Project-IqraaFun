b35fb7e22c794a13b3e6cc27ef9f2df971e261ab
fatihalayyub <naufalabrarrabbanii@gmail.com>
2023-04-27 06:29:13 +0700

    Update pertama

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html', 
+            './node_modules/flowbite/**/*.js',
+            './src/css/Pages/**/*{html,js}',],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#C8AD97',
+      },
+      fontFamily: {
+        body : ['Poppins', 'sans-serif']
+      },
+    },
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1440px',
+  },
+  plugins: 
+  [
+    require('flowbite/plugin'), 
+    require('@tailwindcss/aspect-ratio'),
+  ],
+  },
+};
