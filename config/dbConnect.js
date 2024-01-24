22c218837a1e4e3eac06e6a3f0fefe6f906c3af4
Afidev <110360966+eabdalmufid@users.noreply.github.com>
2024-01-24 08:47:29 +0700

    chore: add token mongodb

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const dbConnect = async () => {
     try {
-        mongoose.connect('mongodb://127.0.0.1:27017/iqrafun')
+        mongoose.connect('mongodb+srv://mongodb-api:Rest-APIs@cluster0.huuao6r.mongodb.net/?retryWrites=true&w=majority') // mongodb://127.0.0.1:27017/iqrafun
         console.log('Database connect...')
     } catch (error) {
         console.log(error)
